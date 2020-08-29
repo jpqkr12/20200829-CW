e0751bab4821e564f7bf0903969a2412098d8a90
Jonathan Park <pjunsoo0412@gmail.com>
2020-08-29 12:48:11 +0800

    print("안뇽")

--- 20200829 CW/ViewController.swift
+++ 20200829 CW/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ni hao!")
+        print("안녕")
         // Do any additional setup after loading the view.
     }
 
